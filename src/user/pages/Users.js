7b98b0e0ad488d31108a90acaf95e442063d0288
Felix Alimin <felixalimin@gmail.com>
2023-12-22 22:03:57 -0500

    added style

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UserList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>users works!</h2>;
+  const USERS = [
+    {
+      id: "ul",
+      name: "Max",
+      image:
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4432582.png&w=350&h=254",
+      places: 3
+    }
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
