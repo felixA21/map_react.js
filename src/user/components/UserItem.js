58e8d5b8eb70e7bee9d9ff5353688d1678c9b435
Felix Alimin <felixalimin@gmail.com>
2023-12-22 22:06:33 -0500

    fixed css

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -7,10 +7,10 @@ const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user_item__image">
+        <div className="user-item__image">
           <Avatar image={props.image} alt={props.name} />
         </div>
-        <div className="user_item__info">
+        <div className="user-item__info">
           <h2>{props.name}</h2>
           <h3>
             {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
