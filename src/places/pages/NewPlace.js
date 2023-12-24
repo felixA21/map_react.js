7e3def6744f30aa7f82a50d6e4ec0b0964393ae4
Felix Alimin <felixalimin@gmail.com>
2023-12-24 16:12:40 -0500

    added validators on input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
 import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
     </form>
   );
 };
