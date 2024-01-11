bd09144eb5ae58b3f6360439bf4810172c45079c
Felix Alimin <felixalimin@gmail.com>
2024-01-11 17:25:42 -0500

    test

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { AuthContext } from "../../context/auth-context";
 
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
+import Button from "../FormElements/Button";
 
 const NavLinks = (props) => {
   const auth = useContext(AuthContext);
@@ -14,19 +15,24 @@ const NavLinks = (props) => {
           ALL USERS
         </NavLink>
       </li>
-      {auth.isLoggedin && (
+      {auth.isLoggedIn && (
         <li>
           <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
       )}
-      {auth.isLoggedin && (
+      {auth.isLoggedIn && (
         <li>
           <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
       )}
-      {!auth.isLoggedin && (
+      {!auth.isLoggedIn && (
         <li>
           <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <Button onClick={auth.logout}>LOGOUT</Button>
         </li>
       )}
     </ul>
