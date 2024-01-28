f74ca9ba1d7014c6e6b65e57595b7d1fcc723850
Felix Alimin <felixalimin@gmail.com>
2024-01-27 21:49:33 -0500

    getting users with get request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UserList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElement/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Max",
-      image:
-        "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4432582.png&w=350&h=254",
-      places: 3
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUser, setLoadedUser] = useState();
 
-  return <UserList items={USERS} />;
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUser(responseData.users);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message);
+      }
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div classname="center">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isLoading && loadedUser && <UserList items={loadedUser} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
